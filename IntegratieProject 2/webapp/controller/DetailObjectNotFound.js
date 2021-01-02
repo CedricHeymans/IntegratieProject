28aa72a472d5a1c6d2e5cb53f8aaf4e028ae83c4
CedricHeymans <60067644+CedricHeymans@users.noreply.github.com>
2021-01-02 16:18:20 +0100

    New project

---
+++ IntegratieProject 2/webapp/controller/DetailObjectNotFound.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"./BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("IntProject.IntegratieProject.controller.DetailObjectNotFound", {});
+});
