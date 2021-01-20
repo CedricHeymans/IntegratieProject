fee7949d87243999fbfb570693db6729f53764dd
CedricHeymans <60067644+CedricHeymans@users.noreply.github.com>
2021-01-20 13:52:46 +0100

    Redo

--- IntegratieProject 2/webapp/controller/DetailObjectNotFound.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-	"./BaseController"
-], function (BaseController) {
-	"use strict";
-
-	return BaseController.extend("IntProject.IntegratieProject.controller.DetailObjectNotFound", {});
-});
